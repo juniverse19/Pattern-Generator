90be4cac1f0b5fbe16510d3aa8d82c8692c3231f
juniverse19 <ryanjkim18@gmail.com>
2025-11-06 15:57:02 +0000

    something

--- Patterns.java
+++ Patterns.java
@@ -1,3 +1,50 @@
+//
+//  Class author:  Ryan
+//  Date created:  11/6/25
+//  General description: a brief summary of what this particular class does.
+//
 public class Patterns {
+    public static void main(String[] args) {
 
+    }
+
+    //
+    //  Pre-condition: what must be true prior to the method being called //  including possible limitations on input
+    //  Post-condition: what the result of executing this method will be.
+    //
+    public static void stars(int rows){
+
+    }
+
+    //
+    //  Pre-condition: what must be true prior to the method being called //  including possible limitations on input
+    //  Post-condition: what the result of executing this method will be.
+    //
+    public static void triangle(int rows){
+
+    }
+
+    //
+    //  Pre-condition: what must be true prior to the method being called //  including possible limitations on input
+    //  Post-condition: what the result of executing this method will be.
+    //
+    public static void odds(int start){
+
+    }
+
+    //
+    //  Pre-condition: what must be true prior to the method being called //  including possible limitations on input
+    //  Post-condition: what the result of executing this method will be.
+    //
+    public static void eo(int maxE){
+
+    }
+
+    //
+    //  Pre-condition: what must be true prior to the method being called //  including possible limitations on input
+    //  Post-condition: what the result of executing this method will be.
+    //
+    public static void pyramid(int rows){
+
+    }
 }
