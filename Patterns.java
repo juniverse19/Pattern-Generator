fff804bae354eb361a3f672624d0eca0331d8d6e
juniverse19 <ryanjkim18@gmail.com>
2025-11-22 00:58:19 +0000

    final

--- Patterns.java
+++ Patterns.java
@@ -1,7 +1,7 @@
 //
-//  Class author:  Ryan
+//  Class author:  Ryan K
 //  Date created:  11/6/25
-//  General description: a brief summary of what this particular class does.
+//  General description: The program Will print out different patterns using letters and symbols
 //
 public class Patterns {
     public static void main(String[] args) {
@@ -9,21 +9,26 @@ public class Patterns {
         stars(7);
         System.out.println();
 
+
         System.out.println("Triangle Pattern (9 rows):");
         triangle(9);
         System.out.println();
+
 
         System.out.println("Odds Pattern (start = 9):");
         odds(9);
         System.out.println();
 
+
         System.out.println("EO Pattern (maxE = 6):");
         eo(6);
         System.out.println();
 
+
         System.out.println("EO Pattern (maxE = 5):");
         eo(5);
         System.out.println();
+
 
         System.out.println("Pyramid Pattern (5 rows):");
         pyramid(5);
@@ -31,8 +36,8 @@ public class Patterns {
     }
 
     //
-    //  Pre-condition: what must be true prior to the method being called //  including possible limitations on input
-    //  Post-condition: what the result of executing this method will be.
+    //  Pre-condition: rows is a positive value
+    //  Post-condition: prints a star pattern with 1, 3, 5, and so on stars up to the determined number of rows
     //
     public static void stars(int rows){
         int i = 1;
@@ -48,8 +53,8 @@ public class Patterns {
     }
 
     //
-    //  Pre-condition: what must be true prior to the method being called //  including possible limitations on input
-    //  Post-condition: what the result of executing this method will be.
+    //  Pre-condition: rows is a positive value
+    //  Post-condition: prints a triangle where each line i contains the digit i repeated i times
     //
     public static void triangle(int rows){
         int i = 1;
@@ -65,8 +70,11 @@ public class Patterns {
     }
 
     //
-    //  Pre-condition: what must be true prior to the method being called //  including possible limitations on input
-    //  Post-condition: what the result of executing this method will be.
+    //  Pre-condition: start is a positive and odd number
+    /* Post-condition: prints a pattern where each line uses the current odd number, 
+    starting from start and decreasing by 2, repeating the number as many times as its 
+    value
+    */
     //
     public static void odds(int start){
         for (int i = start; i >= 1; i -= 2) {
@@ -78,30 +86,30 @@ public class Patterns {
     }
 
     //
-    //  Pre-condition: what must be true prior to the method being called //  including possible limitations on input
-    //  Post-condition: what the result of executing this method will be.
+    //  Pre-condition: maxE is a positive value
+    //  Post-condition: prints patterns of E and O that increases to maxE lines and then decreases
     //
     public static void eo(int maxE){
         int i = 1;
         while (i <= maxE) {
-            char symbol = 'O';
+            char letter = 'O';
             if (i % 2 != 0) {
-                symbol = 'E';
+                letter = 'E';
             }
             for (int j = 1; j <= i; j++) {
-                System.out.print(symbol);
+                System.out.print(letter);
             }
             System.out.println();
             i++;
         }
         i = maxE - 1;
         while (i >= 1) {
-            char symbol = 'O';
+            char letter = 'O';
             if (i % 2 != 0) {
-                symbol = 'E';
+                letter = 'E';
             }
             for (int j = 1; j <= i; j++) {
-                System.out.print(symbol);
+                System.out.print(letter);
             }
             System.out.println();
             i--;
@@ -109,8 +117,8 @@ public class Patterns {
     }
 
     //
-    //  Pre-condition: what must be true prior to the method being called //  including possible limitations on input
-    //  Post-condition: what the result of executing this method will be.
+    //  Pre-condition: rows is a positive value
+    //  Post-condition: prints an inverted number pyramid with each row having spaces between them
     //
     public static void pyramid(int rows){
         for (int i = 1; i <= rows; i++) {
